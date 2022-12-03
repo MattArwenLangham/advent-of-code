0e99ce08feefa3583b399fe751199be199239012
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-12-03 12:38:27 +0000

    rollback start of task 2 in wrong file

--- 2022/day-1/calorie-counting.js
+++ 2022/day-1/calorie-counting.js
@@ -3,17 +3,15 @@ const fs = require('fs')
 const data = fs.readFileSync('./input.txt', {encoding: 'utf8'})
 input = data.split("\r")
 
-const topThree = []
-
 const highestCalories = (input) => {
     let currentCalorieCount = 0
-    let highestCalorieCount = topThree[0]
+    let highestCalorieCount = 0
     for (calories of input){
         calories = parseInt(calories)
     
         if(!calories){
             if (currentCalorieCount > highestCalorieCount){
-                topThree.push(currentCalorieCount)
+                highestCalorieCount = currentCalorieCount
             }
             currentCalorieCount = 0
         } else {
