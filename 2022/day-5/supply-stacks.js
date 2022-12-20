68bd40bf068f012f3e6e5ee932ce8442d4f0e1ed
MattArwenLangham <m.arwen-langham@hotmail.co.uk>
2022-12-20 18:19:08 +0000

    start day 5. All functionality works minus the ordering of the boxes.

---
+++ 2022/day-5/supply-stacks.js
@@ -0,0 +1,49 @@
+//Parse the map.
+//Parse the instructions.
+
+const fs = require('fs')
+
+const data = fs.readFileSync('./test-input.txt', {encoding: 'utf8'})
+const map = data.slice(0, data.indexOf("move"))
+const instructionData = data.slice(data.indexOf("move"))
+const instructions = instructionData.replace(/(move )|(from )|(to )/g, "").replace(/\r\n/g, " ").split(" ")
+const crates = /\w/g
+
+const generateMap = (map) => {
+    const mapArray = [...map.matchAll(crates)]
+    const numOfStacks = parseInt(mapArray[mapArray.length - 1])
+    const numOfCrates = mapArray.length - numOfStacks
+    
+    const startingMap = {}
+
+    for (let i = 0; i < numOfCrates; i++){
+            let crate = mapArray[i][0]
+            let stackNumber = Math.floor(mapArray[i].index / 4) % mapArray[mapArray.length - 1] + 1
+            if(startingMap.hasOwnProperty(stackNumber)){
+                startingMap[stackNumber].push(crate)
+            }
+            else {
+                startingMap[stackNumber] = [crate]
+            }
+    }
+    return startingMap
+}
+
+const moveCrates = (map, instructions) => {
+    const crateMap = generateMap(map)
+    // console.log(crateMap)
+    // console.log(instructions)
+    for(let i = 0; i < instructions.length; i += 3){
+        let quantity = parseInt(instructions[i])
+        let target = instructions[i + 1]
+        let destination = instructions[i + 2]
+        console.log({quantity, target, destination})
+        let liftedCrates = crateMap[target].splice(0, quantity)
+        console.log(liftedCrates)
+        crateMap[destination].unshift(...liftedCrates)
+        console.log(crateMap)
+    }
+    
+}
+
+moveCrates(map, instructions)
