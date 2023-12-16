3305ce24dd44b811475bb6207f6f30232bfb2aea
matt <48326572+MattArwenLangham@users.noreply.github.com>
2023-12-16 14:23:04 +0000

    day 13 clean up print statements

--- 2023_python/day13/point_of_incidence.py
+++ 2023_python/day13/point_of_incidence.py
@@ -3,11 +3,9 @@ def read_input(file_path):
         return file.read().split("\n\n")
 
 def verify_reflection(pattern, i):
-    print("VERIFYING", i , "in", len(pattern))
     mod = -3
     for j in range(i, len(pattern)):
         if j + mod >= 0:
-            print("comparing", j, "and", j + mod, pattern[j], pattern[j + mod])
             if pattern[j] != pattern[j + mod]:
                 return False
         else:
@@ -21,7 +19,6 @@ def find_horizontal_mirror_line(pattern):
         if i == len(pattern_rows) - 1:
             return 0
         elif row == pattern_rows[i + 1]:
-            print("MATCH FOUND", row, pattern_rows[i + 1], "at", i)
             if i == 0 or i == len(pattern) - 2 or verify_reflection(pattern_rows, i + 2):
                 return (i + 1) * 100
 
@@ -31,21 +28,16 @@ def find_vertical_mirror_line(pattern):
         if i == len(pattern_cols) - 1:
             return 0
         elif col == pattern_cols[i + 1]:
-            print("MATCH FOUND", col, pattern_cols[i + 1], "at", i)
             if i == 0 or verify_reflection(pattern_cols, i + 2):
                 return i + 1
 
 def calculate_sum_of_mirrors_in_pattern_map(pattern_map):
     sum = 0
     for pattern in pattern_map:
-        print("---------------")
         to_add = 0
         to_add += find_horizontal_mirror_line(pattern)
-        if to_add:
-            print("FOUND adding", to_add)
         if not to_add:
             to_add += find_vertical_mirror_line(pattern)
-            print("FOUND adding", to_add)
         sum += to_add
     return sum
 
