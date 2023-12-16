cbcce62a7d0e0d94d40053c1aca3db1a77255379
matt <48326572+MattArwenLangham@users.noreply.github.com>
2023-12-16 14:23:44 +0000

    day 14 cleanup solution

--- 2023_python/day14/parabolic.py
+++ 2023_python/day14/parabolic.py
@@ -1,6 +1,6 @@
-def read_input():
-    f = open("input.txt", "r")
-    return f.read().split("\n")
+def read_input(file_path):
+    with open(file_path, 'r') as file:
+        return file.read().split("\n")
 
 movable = 'O'
 immovable = '#'
@@ -8,18 +8,17 @@ immovable = '#'
 def tilt_up(rock_map):
     sum_load = 0
     max_load_num = len(rock_map)
-    last_empty_space = [1 if rock == immovable else 0 for rock in rock_map[0]]
+    last_empty_row_in_col = [1 if rock == immovable else 0 for rock in rock_map[0]]
 
-    for line, rock_line in enumerate(rock_map):
-        for i, rock in enumerate(rock_line):
+    for row, rock_row in enumerate(rock_map):
+        for col, rock in enumerate(rock_row):
             if rock == movable:
-                sum_load += max_load_num - last_empty_space[i]
-                last_empty_space[i] += 1
+                sum_load += max_load_num - last_empty_row_in_col[col]
+                last_empty_row_in_col[col] += 1
             elif rock == immovable:
-                last_empty_space[i] = line + 1
-
+                last_empty_row_in_col[col] = row + 1
     return sum_load
 
-rock_map = read_input()
+rock_map = read_input('input.txt')
 result = tilt_up(rock_map)
 print(result)
