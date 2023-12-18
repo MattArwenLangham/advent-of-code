3167c390c688af1eca5546947807217f5b1196d8
matt <48326572+MattArwenLangham@users.noreply.github.com>
2023-12-18 21:14:29 +0000

    day 13 extra redo unfinished

---
+++ 2023_python/day13/point_of_incidence_extra.py
@@ -0,0 +1,48 @@
+def read_input(file_path):
+    with open(file_path, 'r') as file:
+        return file.read().split("\n\n")
+
+differences = 0
+
+def verify_reflection(pattern, i):
+    mod = -3
+    for j in range(i, len(pattern)):
+        if j + mod >= 0:
+            if pattern[j] != pattern[j + mod]:
+                return False
+        else:
+            return True
+        mod -= 2
+    return True
+
+def find_horizontal_mirror_line(pattern):
+    pattern_rows = pattern.split("\n")
+    for i, row in enumerate(pattern_rows):
+        if i == len(pattern_rows) - 1:
+            return 0
+        elif row == pattern_rows[i + 1]:
+            if i == 0 or i == len(pattern) - 2 or verify_reflection(pattern_rows, i + 2):
+                return (i + 1) * 100
+
+def find_vertical_mirror_line(pattern):
+    pattern_cols = list(map(list, zip(*pattern.split("\n"))))
+    for i, col in enumerate(pattern_cols):
+        if i == len(pattern_cols) - 1:
+            return 0
+        elif col == pattern_cols[i + 1]:
+            if i == 0 or verify_reflection(pattern_cols, i + 2):
+                return i + 1
+
+def calculate_sum_of_mirrors_in_pattern_map(pattern_map):
+    sum = 0
+    for pattern in pattern_map:
+        to_add = 0
+        to_add += find_horizontal_mirror_line(pattern)
+        if not to_add:
+            to_add += find_vertical_mirror_line(pattern)
+        sum += to_add
+    return sum
+
+pattern_map = read_input("input.txt")
+result = calculate_sum_of_mirrors_in_pattern_map(pattern_map)
+print(result)
